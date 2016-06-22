3e9bae03a2d65d5a82aebaf281a6792ee757149b
Jason Taylor <jason@redant.com.au>
2016-06-22 16:55:47 +1000

    Default react_on_rails redux install

--- client/app/bundles/HelloWorld/containers/HelloWorld.jsx
+++ client/app/bundles/HelloWorld/containers/HelloWorld.jsx
@@ -1,29 +1,42 @@
 import React, { PropTypes } from 'react';
 import HelloWorldWidget from '../components/HelloWorldWidget';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import Immutable from 'immutable';
+import * as helloWorldActionCreators from '../actions/helloWorldActionCreators';
+
+function select(state) {
+  // Which part of the Redux global state does our component want to receive as props?
+  // Note the use of `$$` to prefix the property name because the value is of type Immutable.js
+  return { $$helloWorldStore: state.$$helloWorldStore };
+}
 
 // Simple example of a React "smart" component
-export default class HelloWorld extends React.Component {
-  static propTypes = {
-    name: PropTypes.string.isRequired, // this is passed from the Rails view
-  };
+const HelloWorld = (props) => {
+  const { dispatch, $$helloWorldStore } = props;
+  const actions = bindActionCreators(helloWorldActionCreators, dispatch);
+  const { updateName } = actions;
+  const name = $$helloWorldStore.get('name');
 
-  constructor(props, context) {
-    super(props, context);
+  // This uses the ES2015 spread operator to pass properties as it is more DRY
+  // This is equivalent to:
+  // <HelloWorldWidget $$helloWorldStore={$$helloWorldStore} actions={actions} />
+  return (
+    <HelloWorldWidget {...{ updateName, name }} />
+  );
+};
 
-    // How to set initial state in ES6 class syntax
-    // https://facebook.github.io/react/docs/reusable-components.html#es6-classes
-    this.state = { name: this.props.name };
-  }
+HelloWorld.propTypes = {
+  dispatch: PropTypes.func.isRequired,
 
-  updateName(name) {
-    this.setState({ name });
-  }
+  // This corresponds to the value used in function select above.
+  // We prefix all property and variable names pointing to Immutable.js objects with '$$'.
+  // This allows us to immediately know we don't call $$helloWorldStore['someProperty'], but
+  // instead use the Immutable.js `get` API for Immutable.Map
+  $$helloWorldStore: PropTypes.instanceOf(Immutable.Map).isRequired,
+};
 
-  render() {
-    return (
-      <div>
-        <HelloWorldWidget name={this.state.name} updateName={e => this.updateName(e)} />
-      </div>
-    );
-  }
-}
+// Don't forget to actually use connect!
+// Note that we don't export HelloWorld, but the redux "connected" version of it.
+// See https://github.com/reactjs/react-redux/blob/master/docs/api.md#examples
+export default connect(select)(HelloWorld);
